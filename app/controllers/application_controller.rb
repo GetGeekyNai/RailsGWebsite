7da7d97e50dac08821b3c1f4d5ef2e896bea5fef
Harrisonkamau <kamauharrison87@gmail.com>
2018-05-07 13:30:15 +0300

    Ensure only authenticated users access home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
+
+  def authenticate_user
+    redirect_to login_path unless @current_user
+  end
 end
